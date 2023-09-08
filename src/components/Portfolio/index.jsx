3816a8db1359dd306062e68d92e9172318de3172
byronallan <127366720+byronallan@users.noreply.github.com>
2023-09-08 15:37:00 -0700

    almost finished

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -11,11 +11,11 @@ function Portfolio() {
 
         {/* PinPoint */}
         <div className='row d-flex align-items-center justify-content-center'>
-        <h2 className='header d-flex justify-content-start'>PinPoint</h2>
+        <h2 className='header d-flex justify-content-start'>Pin Point</h2>
           <img className='glow col-12 rounded border-color isabelline-bg p-3 gif' src="src/components/Portfolio/PinPoint.gif" alt="PinPoint" />
           <div className='column'>
             <h3>
-              <a href="https://pinpoint-davis-383be2067b3d.herokuapp.com/" target="_blank" rel="noopener noreferrer">PinPoint</a>
+              <a href="https://pinpoint-davis-383be2067b3d.herokuapp.com/" target="_blank" rel="noopener noreferrer">Pin Point</a>
             </h3>
             <a href="https://github.com/byronontheboard/pin-point" target="_blank" rel="noopener noreferrer">
               <Icon></Icon>
@@ -23,13 +23,15 @@ function Portfolio() {
           </div>
         </div>
 
+        <hr />
+
         {/* ChoreChampion */}
         <div className='row d-flex align-items-center justify-content-center'>
-        <h2 className='header d-flex justify-content-start'>ChoreChampion</h2>
-          <img className='glow col-12 rounded border-color isabelline-bg p-3 gif' src="src/components/Portfolio/CHORECHAMPION.gif" alt="ChoreChampion" />
+        <h2 className='header d-flex justify-content-start'>Chore Champion</h2>
+          <img className='glow col-12 rounded border-color isabelline-bg p-3 gif' src="src/components/Portfolio/CHORECHAMPION.gif" alt="Chore Champion" />
           <div className='column'>
             <h3>
-              <a href="https://chore-champion-6838d87c31f6.herokuapp.com/login" target="_blank" rel="noopener noreferrer">ChoreChampion</a>
+              <a href="https://chore-champion-6838d87c31f6.herokuapp.com/login" target="_blank" rel="noopener noreferrer">Chore Champion</a>
             </h3>
             <a href="https://github.com/byronontheboard/chore-champion" target="_blank" rel="noopener noreferrer">
               <Icon></Icon>
@@ -37,6 +39,68 @@ function Portfolio() {
           </div>
         </div>
 
+        <hr />
+
+        {/* Password Generator */}
+        <div className='row d-flex align-items-center justify-content-center'>
+        <h2 className='header d-flex justify-content-start'>Password Generator</h2>
+          <img className='glow col-12 rounded border-color isabelline-bg p-3 gif' src="src/components/Portfolio/Byron's Password Generator.gif" alt="Password Generator" />
+          <div className='column'>
+            <h3>
+              <a href="https://byronontheboard.github.io/js-password-generator/" target="_blank" rel="noopener noreferrer">Password Generator</a>
+            </h3>
+            <a href="https://github.com/byronontheboard/js-password-generator" target="_blank" rel="noopener noreferrer">
+              <Icon></Icon>
+            </a>
+          </div>
+        </div>
+
+        <hr />
+        {/* Note Taker */}
+        <div className='row d-flex align-items-center justify-content-center'>
+        <h2 className='header d-flex justify-content-start'>Note Taker</h2>
+          <img className='glow col-12 rounded border-color isabelline-bg p-3 gif' src="src/components/Portfolio/Note-Taker (1).gif" alt="Note Taker" />
+          <div className='column'>
+            <h3>
+              <a href="https://byronontheboard.github.io/js-password-generator/" target="_blank" rel="noopener noreferrer">Note Taker</a>
+            </h3>
+            <a href="https://github.com/byronontheboard/js-password-generator" target="_blank" rel="noopener noreferrer">
+              <Icon></Icon>
+            </a>
+          </div>
+        </div>
+
+        <hr />
+
+        {/* Retro JS Quiz */}
+        <div className='row d-flex align-items-center justify-content-center'>
+        <h2 className='header d-flex justify-content-start'>Coding Quiz</h2>
+          <img className='glow col-12 rounded border-color isabelline-bg p-3' src="src/components/Portfolio/__WHAT IS CODING_ _ QUIZ.gif" alt="Coding Quiz" />
+          <div className='column'>
+            <h3>
+              <a href="https://byronontheboard.github.io/my-coding-quiz/" target="_blank" rel="noopener noreferrer">What Is Coding?</a>
+            </h3>
+            <a href="https://github.com/byronontheboard/my-coding-quiz" target="_blank" rel="noopener noreferrer">
+              <Icon></Icon>
+            </a>
+          </div>
+        </div>
+
+        <hr />
+
+        {/* Password Generator */}
+        <div className='row d-flex align-items-center justify-content-center'>
+        <h2 className='header d-flex justify-content-start'>Password Generator</h2>
+          <img className='glow col-12 rounded border-color isabelline-bg p-3 gif' src="src/components/Portfolio/Byron's Password Generator.gif" alt="PasswordGenerator" />
+          <div className='column'>
+            <h3>
+              <a href="https://byronontheboard.github.io/js-password-generator/" target="_blank" rel="noopener noreferrer">Password Generator</a>
+            </h3>
+            <a href="https://github.com/byronontheboard/js-password-generator" target="_blank" rel="noopener noreferrer">
+              <Icon></Icon>
+            </a>
+          </div>
+        </div>
 
         <hr />
     </section>
